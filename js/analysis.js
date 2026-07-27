67d2978304d5ca7161d6f5560ab4267126657f7f
hsiaocharlie-sketch <hsiao.charlie@gmail.com>
2026-07-27 11:28:00 +0800

    更新 analysis.js

--- js/analysis.js
+++ js/analysis.js
@@ -1,75 +1,14 @@
-const elementMap = {
-
-"甲":"木",
-"乙":"木",
-
-"丙":"火",
-"丁":"火",
-
-"戊":"土",
-"己":"土",
-
-"庚":"金",
-"辛":"金",
-
-"壬":"水",
-"癸":"水"
-
-};
-
+/*
+八字综合分析入口
+*/
 
 
 function analyzeBazi(bazi){
 
 
-let dayMaster =
-bazi.dayMaster;
 
-
-let element =
-elementMap[dayMaster];
-
-
-
-let reasons=[];
-
-
-
-if(element==="水"){
-
-
-reasons=[
-
-"日主为癸水",
-
-"生于丑月，水有余气，但寒湿之气较重",
-
-"地支亥卯组合，水木关系明显",
-
-"月干乙木、时干乙木泄水生木"
-
-];
-
-
-}
-
-
-
-else{
-
-
-reasons=[
-
-"日主五行："+element,
-
-"当前分析规则尚未展开"
-
-];
-
-
-}
-
-
+let strength =
+calculateStrength(bazi);
 
 
 
@@ -78,59 +17,17 @@ return {
 
 dayMaster:{
 
-stem:dayMaster,
+stem:bazi.dayMaster,
 
-element:element
-
-},
-
-
-
-wuxing:{
-
-
-wood:30,
-
-fire:10,
-
-earth:25,
-
-metal:15,
-
-water:40
-
+element:getWuxing(
+bazi.dayMaster
+)
 
 },
 
 
 
-strength:{
-
-
-level:"待综合判断",
-
-
-score:50,
-
-
-reasons:reasons
-
-
-},
-
-
-
-useGod:{
-
-
-primary:"待分析",
-
-secondary:"待分析",
-
-avoid:"待分析"
-
-
-}
+strength:strength
 
 
 
