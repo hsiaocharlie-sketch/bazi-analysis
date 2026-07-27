b1568446d02c928197fd324aaa823ca760a7631a
hsiaocharlie-sketch <hsiao.charlie@gmail.com>
2026-07-27 12:29:07 +0800

    更新 report.js

--- js/report.js
+++ js/report.js
@@ -6,182 +6,22 @@
 function createReport(bazi,result){
 
 
-let html = "";
+let html="";
 
 
-html += createPillarCard(bazi);
+html+=createPillarCard(bazi);
 
 
-html += createDayMasterCard(result);
+html+=createDayMasterCard(result);
 
 
-html += createWuxingCard(result.wuxing);
+html+=createStructureCard(result.structure);
 
 
-html += createStrengthCard(result.strength);
+html+=createWuxingCard(result.wuxing);
 
 
-
-return html;
-
-
-}
-
-
-
-
-
-function createPillarCard(bazi){
-
-
-
-let pillars = [
-
-{
-name:"年柱",
-data:bazi.pillars.year
-},
-
-{
-name:"月柱",
-data:bazi.pillars.month
-},
-
-{
-name:"日柱",
-data:bazi.pillars.day
-},
-
-{
-name:"时柱",
-data:bazi.pillars.hour
-}
-
-];
-
-
-
-let html = `
-
-<section class="report-card">
-
-<h2>
-四柱命盘
-</h2>
-
-
-<div class="bazi-table">
-
-
-<div class="row title-row">
-
-<div></div>
-<div>天干</div>
-<div>地支</div>
-
-</div>
-
-`;
-
-
-
-pillars.forEach(p=>{
-
-
-let data=p.data;
-
-
-
-let hidden="";
-
-
-if(data.hidden){
-
-hidden=data.hidden
-.map(x=>x.gan)
-.join(" ");
-
-}
-
-
-
-html += `
-
-
-<div class="row">
-
-
-<div class="pillar-name">
-
-${p.name}
-
-</div>
-
-
-
-<div class="pillar-cell">
-
-<div class="gan">
-
-${data.gan}
-
-</div>
-
-
-<div class="god">
-
-${data.tenGod || ""}
-
-</div>
-
-
-</div>
-
-
-
-
-
-<div class="pillar-cell">
-
-
-<div class="zhi">
-
-${data.zhi}
-
-</div>
-
-
-
-<div class="hidden">
-
-藏干：
-
-${hidden}
-
-</div>
-
-
-</div>
-
-
-
-</div>
-
-
-`;
-
-
-});
-
-
-
-html += `
-
-</div>
-
-</section>
-
-`;
+html+=createStrengthCard(result.strength);
 
 
 
@@ -195,178 +35,44 @@ return html;
 
 
 
-
-function createDayMasterCard(result){
+function createStructureCard(structure){
 
 
 return `
 
-<section class="report-card">
-
-
-<h2>
-日主
-</h2>
-
-
-<div class="daymaster">
-
-${result.dayMaster.stem}
-
-<span>
-${result.dayMaster.element}
-</span>
-
-
-</div>
-
-
-</section>
-
-`;
-
-}
-
-
-
-
-
-
-
-function createWuxingCard(wuxing){
-
-
-if(!wuxing){
-
-return "";
-
-}
-
-
-
-let items=[
-"木",
-"火",
-"土",
-"金",
-"水"
-];
-
-
-let html=`
-
-
-<section class="report-card">
-
-<h2>
-五行力量
-</h2>
-
-`;
-
-
-
-items.forEach(item=>{
-
-
-html += `
-
-
-<div class="five-row">
-
-
-<div class="five-name">
-
-${item}
-
-</div>
-
-
-<div class="bar-box">
-
-<div class="bar"
-
-style="width:${wuxing[item]}%">
-
-</div>
-
-</div>
-
-
-<div>
-
-${wuxing[item]}%
-
-</div>
-
-
-</div>
-
-
-`;
-
-
-
-});
-
-
-
-html += `
-
-</section>
-
-`;
-
-
-
-return html;
-
-
-}
-
-
-
-
-
-
-
-function createStrengthCard(strength){
-
-
-
-return `
 
 <section class="report-card">
 
 
 <h2>
-旺衰判断
+格局分析
 </h2>
 
 
-<div class="strength-title">
+<h3>
 
-${strength.level}
+${structure.structure}
 
-</div>
-
-
-<div>
-
-评分：
-${strength.score}
-
-</div>
+</h3>
 
 
 <p>
 
-${strength.reasons.join("<br>")}
+月令：
+${structure.monthZhi}
+
+</p>
+
+
+<p>
+
+${structure.description}
 
 </p>
 
 
 </section>
+
 
 `;
 
