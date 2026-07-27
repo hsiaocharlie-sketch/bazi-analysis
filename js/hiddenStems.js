a1e4ade8636192167a6a49dac27a5370b1fdbbc9
hsiaocharlie-sketch <hsiao.charlie@gmail.com>
2026-07-27 11:23:53 +0800

    创建 hiddenStems.js

---
+++ js/hiddenStems.js
@@ -0,0 +1,168 @@
+/*
+十二地支藏干
+*/
+
+const hiddenStems = {
+
+
+"子":[
+    {
+        gan:"癸",
+        strength:100
+    }
+],
+
+
+"丑":[
+    {
+        gan:"己",
+        strength:60
+    },
+    {
+        gan:"癸",
+        strength:30
+    },
+    {
+        gan:"辛",
+        strength:10
+    }
+],
+
+
+"寅":[
+    {
+        gan:"甲",
+        strength:60
+    },
+    {
+        gan:"丙",
+        strength:30
+    },
+    {
+        gan:"戊",
+        strength:10
+    }
+],
+
+
+"卯":[
+    {
+        gan:"乙",
+        strength:100
+    }
+],
+
+
+"辰":[
+    {
+        gan:"戊",
+        strength:60
+    },
+    {
+        gan:"乙",
+        strength:30
+    },
+    {
+        gan:"癸",
+        strength:10
+    }
+],
+
+
+"巳":[
+    {
+        gan:"丙",
+        strength:60
+    },
+    {
+        gan:"戊",
+        strength:30
+    },
+    {
+        gan:"庚",
+        strength:10
+    }
+],
+
+
+"午":[
+    {
+        gan:"丁",
+        strength:70
+    },
+    {
+        gan:"己",
+        strength:30
+    }
+],
+
+
+"未":[
+    {
+        gan:"己",
+        strength:60
+    },
+    {
+        gan:"丁",
+        strength:30
+    },
+    {
+        gan:"乙",
+        strength:10
+    }
+],
+
+
+"申":[
+    {
+        gan:"庚",
+        strength:60
+    },
+    {
+        gan:"壬",
+        strength:30
+    },
+    {
+        gan:"戊",
+        strength:10
+    }
+],
+
+
+"酉":[
+    {
+        gan:"辛",
+        strength:100
+    }
+],
+
+
+"戌":[
+    {
+        gan:"戊",
+        strength:60
+    },
+    {
+        gan:"辛",
+        strength:30
+    },
+    {
+        gan:"丁",
+        strength:10
+    }
+],
+
+
+"亥":[
+    {
+        gan:"壬",
+        strength:70
+    },
+    {
+        gan:"甲",
+        strength:30
+    }
+]
+
+
+};
