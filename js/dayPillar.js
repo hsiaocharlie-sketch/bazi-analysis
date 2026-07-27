1cb5fb79002941a26527b878b865af9dddbb29fb
hsiaocharlie-sketch <hsiao.charlie@gmail.com>
2026-07-27 16:43:56 +0800

    更新 dayPillar.js

--- js/dayPillar.js
+++ js/dayPillar.js
@@ -1,7 +1,12 @@
 /*
-日柱
+日柱计算模块
 
-1984-01-10 = 癸卯
+六十甲子计算
+
+基准：
+2000年1月1日
+庚辰日
+
 */
 
 
@@ -9,66 +14,69 @@ function calculateDayPillar(year,month,day){
 
 
 
-let jd =
-calculateJD(
-year,
-month,
-day
-);
+    let baseDate =
+    new Date(
+        "2000-01-01T00:00:00"
+    );
 
 
 
-let index =
-(
-Math.floor(jd-2445709.5)
-+
-39
-)
-%60;
+    let targetDate =
+    new Date(
+        year+"-"+month+"-"+day+"T00:00:00"
+    );
 
 
 
-return {
-
-
-gan:
-heavenlyStems[index%10],
-
-
-zhi:
-earthlyBranches[index%12]
-
-
-};
-
-
-}
+    let diff =
+    Math.floor(
+        (
+            targetDate - baseDate
+        )
+        /
+        86400000
+    );
 
 
 
+    /*
+    2000-01-01 = 庚辰
 
-function calculateJD(year,month,day){
+    庚辰在六十甲子中：
+    index 16
 
-
-if(month<=2){
-
-year--;
-month+=12;
-
-}
+    */
 
 
-let A=Math.floor(year/100);
-
-let B=2-A+Math.floor(A/4);
+    let index =
+    (16 + diff) % 60;
 
 
 
-return Math.floor(365.25*(year+4716))
-+
-Math.floor(30.6001*(month+1))
-+
-day+B-1524.5;
+    if(index<0){
+
+        index += 60;
+
+    }
+
+
+
+    return {
+
+
+        gan:
+        heavenlyStems[
+            index % 10
+        ],
+
+
+        zhi:
+        earthlyBranches[
+            index % 12
+        ]
+
+
+    };
 
 
 }
