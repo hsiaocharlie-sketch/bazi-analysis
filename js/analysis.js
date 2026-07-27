3b80f2dcf68cfb0fa56bd3a12f46ea6e8734484a
hsiaocharlie-sketch <hsiao.charlie@gmail.com>
2026-07-27 15:39:34 +0800

    更新 analysis.js

--- js/analysis.js
+++ js/analysis.js
@@ -7,85 +7,94 @@ function analyzeBazi(bazi){
 
 
 
-    if(typeof addTenGodToPillars === "function"){
+if(typeof addTenGodToPillars==="function"){
 
-        addTenGodToPillars(bazi);
+addTenGodToPillars(bazi);
 
-    }
-
-
-
-    let strength = null;
-
-
-    if(typeof calculateStrength === "function"){
-
-        strength =
-        calculateStrength(bazi);
-
-    }
+}
 
 
 
-    let wuxing = null;
+let strength =
+calculateStrength(bazi);
 
 
-    if(typeof calculateWuxingStrength === "function"){
 
-        wuxing =
-        calculateWuxingStrength(bazi);
-
-    }
+let wuxing =
+calculateWuxingStrength(bazi);
 
 
 
 
-    let structure = null;
+let structure=null;
 
 
-    if(typeof analyzeStructure === "function"){
-
-        structure =
-        analyzeStructure(bazi);
-
-    }
+if(typeof analyzeStructure==="function"){
 
 
+structure=
+analyzeStructure(bazi);
 
 
-
-    return {
-
-
-        dayMaster:{
-
-
-            stem:bazi.dayMaster,
-
-
-            element:
-            getWuxing(
-                bazi.dayMaster
-            )
-
-
-        },
+}
 
 
 
-        strength:strength,
+
+let useGod=null;
+
+
+if(typeof analyzeUseGod==="function"){
+
+
+useGod=
+analyzeUseGod(
+bazi,
+strength
+);
+
+
+}
 
 
 
-        wuxing:wuxing,
+
+return {
+
+
+dayMaster:{
+
+
+stem:bazi.dayMaster,
+
+
+element:
+getWuxing(
+bazi.dayMaster
+)
+
+
+},
 
 
 
-        structure:structure
+strength:strength,
 
 
 
-    };
+wuxing:wuxing,
+
+
+
+structure:structure,
+
+
+
+useGod:useGod
+
+
+
+};
 
 
 }
