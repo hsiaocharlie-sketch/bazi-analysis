2b12174d780ee2462b5808e224d57a9762463f52
hsiaocharlie-sketch <hsiao.charlie@gmail.com>
2026-07-27 11:48:18 +0800

    更新 monthPillar.js

--- js/monthPillar.js
+++ js/monthPillar.js
@@ -1,104 +1,147 @@
-const monthBranches = [
+/*
+月柱计算
 
-"寅",
-"卯",
-"辰",
-"巳",
-"午",
-"未",
-"申",
-"酉",
-"戌",
-"亥",
-"子",
-"丑"
+当前采用节气月框架
+*/
 
-];
+
+function calculateMonthPillar(date,yearGan){
 
 
 
-const monthStemTable = {
+let month =
+date.getMonth()+1;
 
 
-"甲":"丙",
-"己":"丙",
-
-
-"乙":"戊",
-"庚":"戊",
-
-
-"丙":"庚",
-"辛":"庚",
-
-
-"丁":"壬",
-"壬":"壬",
-
-
-"戊":"甲",
-"癸":"甲"
-
-
-};
+let branch;
 
 
 
-const stemIndex = {
+if(month===1){
 
-"甲":0,
-"乙":1,
-"丙":2,
-"丁":3,
-"戊":4,
-"己":5,
-"庚":6,
-"辛":7,
-"壬":8,
-"癸":9
+branch="丑";
 
-};
+}
+else if(month===2 || month===3){
+
+branch="寅";
+
+}
+else if(month===4){
+
+branch="卯";
+
+}
+else if(month===5){
+
+branch="辰";
+
+}
+else if(month===6){
+
+branch="巳";
+
+}
+else if(month===7){
+
+branch="午";
+
+}
+else if(month===8){
+
+branch="未";
+
+}
+else if(month===9){
+
+branch="申";
+
+}
+else if(month===10){
+
+branch="酉";
+
+}
+else if(month===11){
+
+branch="戌";
+
+}
+else{
+
+branch="亥";
+
+}
 
 
 
-const stems=[
+/*
+五虎遁
 
-"甲",
-"乙",
-"丙",
-"丁",
-"戊",
-"己",
-"庚",
-"辛",
-"壬",
-"癸"
+甲己年 丙寅月
+乙庚年 戊寅月
+丙辛年 庚寅月
+丁壬年 壬寅月
+戊癸年 甲寅月
 
-];
+*/
 
 
-
-function calculateMonthPillar(yearStem,solarMonth){
+let start;
 
 
 
-let branch =
-monthBranches[solarMonth-1];
+if(
+yearGan==="甲" ||
+yearGan==="己"
+){
+
+start=2;
+
+}
+
+else if(
+yearGan==="乙" ||
+yearGan==="庚"
+){
+
+start=4;
+
+}
+
+else if(
+yearGan==="丙" ||
+yearGan==="辛"
+){
+
+start=6;
+
+}
+
+else if(
+yearGan==="丁" ||
+yearGan==="壬"
+){
+
+start=8;
+
+}
+
+else{
+
+start=0;
+
+}
 
 
 
-let startStem =
-monthStemTable[yearStem];
+let branchIndex =
+earthlyBranches.indexOf(branch);
 
 
 
-let startIndex =
-stemIndex[startStem];
-
-
-
-let index =
-(startIndex + solarMonth -1)%10;
+let offset =
+(branchIndex-2+12)%12;
 
 
 
@@ -106,7 +149,9 @@ return {
 
 
 gan:
-stems[index],
+heavenlyStems[
+(start+offset)%10
+],
 
 
 zhi:
