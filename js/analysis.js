5b555a91ac39ea73dbe56741da978c8f7b29a191
hsiaocharlie-sketch <hsiao.charlie@gmail.com>
2026-07-27 09:32:32 +0800

    更新 analysis.js

--- js/analysis.js
+++ js/analysis.js
@@ -1,60 +1,68 @@
 function analyzeBazi(bazi){
 
 
-
-let result={};
-
+return {
 
 
-result.wuxing={
-
-木:25,
-
-火:40,
-
-土:10,
-
-金:20,
-
-水:5
-
-};
+wuxing:{
 
 
+wood:30,
 
-result.strength={
+fire:40,
 
-score:42,
+earth:15,
+
+metal:25,
+
+water:10
+
+
+},
+
+
+
+strength:{
+
 
 level:"偏弱",
 
-reason:[
+
+score:42,
+
+
+reasons:[
 
 "生于巳月，火旺泄木",
 
-"日主得辰根",
+"日坐辰，有余气根",
 
-"时干壬水透出，有印生扶"
+"时干壬水透出，可以生扶日主"
 
 ]
 
-};
+
+},
 
 
 
-result.useGod={
+useGod:{
+
 
 primary:"水",
 
+
 secondary:"木",
+
 
 avoid:"火过旺"
 
+
+}
+
+
+
 };
-
-
-
-return result;
 
 
 }
