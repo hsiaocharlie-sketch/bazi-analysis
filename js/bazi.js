67bbe219c6c50e71b42d432d9310b5fb86a03379
hsiaocharlie-sketch <hsiao.charlie@gmail.com>
2026-07-27 09:53:35 +0800

    更新 bazi.js

--- js/bazi.js
+++ js/bazi.js
@@ -14,32 +14,30 @@ const earthlyBranches = [
 function calculateYear(year,month,day){
 
 
-/*
-暂用立春前后判断框架
-后续加入太阳黄经计算
-*/
 
+if(
+month<2 ||
+(month===2 && day<4)
+){
 
-let y=year;
-
-
-if(month<2 || (month===2 && day<4)){
-
-y=y-1;
+year--;
 
 }
 
 
 
-let index=(y-4)%10;
+let ganIndex=(year-4)%10;
 
-if(index<0) index+=10;
+let zhiIndex=(year-4)%12;
 
 
-let branchIndex=(y-4)%12;
 
-if(branchIndex<0)
-branchIndex+=12;
+if(ganIndex<0)
+ganIndex+=10;
+
+
+if(zhiIndex<0)
+zhiIndex+=12;
 
 
 
@@ -47,11 +45,11 @@ return {
 
 
 gan:
-heavenlyStems[index],
+heavenlyStems[ganIndex],
 
 
 zhi:
-earthlyBranches[branchIndex]
+earthlyBranches[zhiIndex]
 
 
 };
@@ -63,46 +61,61 @@ earthlyBranches[branchIndex]
 
 
 
+
 function calculateHour(hour){
+
 
 
 let branch;
 
 
+
 if(hour>=23 || hour<1)
+
 branch="子";
 
 else if(hour<3)
+
 branch="丑";
 
 else if(hour<5)
+
 branch="寅";
 
 else if(hour<7)
+
 branch="卯";
 
 else if(hour<9)
+
 branch="辰";
 
 else if(hour<11)
+
 branch="巳";
 
 else if(hour<13)
+
 branch="午";
 
 else if(hour<15)
+
 branch="未";
 
 else if(hour<17)
+
 branch="申";
 
 else if(hour<19)
+
 branch="酉";
 
 else if(hour<21)
+
 branch="戌";
 
 else
+
 branch="亥";
 
 
@@ -122,7 +135,9 @@ zhi:branch
 
 
 
+
 function calculateBazi(input){
+
 
 
 let date =
@@ -150,6 +165,34 @@ date.getHours();
 
 
 
+let yearPillar =
+calculateYear(
+year,
+month,
+day
+);
+
+
+
+let solarMonth =
+getSolarMonth(date);
+
+
+
+let monthPillar =
+calculateMonthPillar(
+yearPillar.gan,
+solarMonth
+);
+
+
+
+let hourPillar =
+calculateHour(hour);
+
+
+
+
 return {
 
 
@@ -160,22 +203,11 @@ pillars:{
 
 
 year:
-calculateYear(
-year,
-month,
-day
-),
+yearPillar,
 
 
-
-month:{
-
-gan:"待计算",
-
-zhi:"待计算"
-
-},
-
+month:
+monthPillar,
 
 
 day:{
@@ -187,16 +219,16 @@ zhi:"待计算"
 },
 
 
-
 hour:
-calculateHour(hour)
+hourPillar
 
 
 },
 
 
 
-dayMaster:"待计算"
+dayMaster:
+"待计算"
 
 
 };
