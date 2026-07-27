7f97f61043b247558564e376c83da55c1a28749c
hsiaocharlie-sketch <hsiao.charlie@gmail.com>
2026-07-27 12:01:30 +0800

    更新 hourPillar.js

--- js/hourPillar.js
+++ js/hourPillar.js
@@ -1,81 +1,16 @@
 /*
-时柱计算
+时柱
 五鼠遁
 */
 
 
-function calculateHourPillar(
-hour,
-dayGan
-){
+function calculateHourPillar(hour,dayGan){
 
 
-
-let branchIndex;
-
-
-
-if(hour>=23 || hour<1){
-
-branchIndex=0;
-
-}
-else if(hour<3){
-
-branchIndex=1;
-
-}
-else if(hour<5){
-
-branchIndex=2;
-
-}
-else if(hour<7){
-
-branchIndex=3;
-
-}
-else if(hour<9){
-
-branchIndex=4;
-
-}
-else if(hour<11){
-
-branchIndex=5;
-
-}
-else if(hour<13){
-
-branchIndex=6;
-
-}
-else if(hour<15){
-
-branchIndex=7;
-
-}
-else if(hour<17){
-
-branchIndex=8;
-
-}
-else if(hour<19){
-
-branchIndex=9;
-
-}
-else if(hour<21){
-
-branchIndex=10;
-
-}
-else{
-
-branchIndex=11;
-
-}
-
+let branchIndex=
+Math.floor(
+((hour+1)%24)/2
+);
 
 
 
@@ -83,34 +18,22 @@ let start;
 
 
 
-if(
-dayGan==="甲" ||
-dayGan==="己"
-){
+if(dayGan==="甲"||dayGan==="己"){
 
 start=0;
 
 }
-else if(
-dayGan==="乙" ||
-dayGan==="庚"
-){
+else if(dayGan==="乙"||dayGan==="庚"){
 
 start=2;
 
 }
-else if(
-dayGan==="丙" ||
-dayGan==="辛"
-){
+else if(dayGan==="丙"||dayGan==="辛"){
 
 start=4;
 
 }
-else if(
-dayGan==="丁" ||
-dayGan==="壬"
-){
+else if(dayGan==="丁"||dayGan==="壬"){
 
 start=6;
 
@@ -120,6 +43,7 @@ else{
 start=8;
 
 }
+
 
 
 
