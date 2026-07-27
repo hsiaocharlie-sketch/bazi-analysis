dd3642c08e958ce3630784d9dcaa3078d7ace6b7
hsiaocharlie-sketch <hsiao.charlie@gmail.com>
2026-07-27 12:32:05 +0800

    更新 report.js

--- js/report.js
+++ js/report.js
@@ -9,19 +9,42 @@ function createReport(bazi,result){
 let html="";
 
 
-html+=createPillarCard(bazi);
+
+html += createPillarCard(bazi);
 
 
-html+=createDayMasterCard(result);
+
+html += createDayMasterCard(result);
 
 
-html+=createStructureCard(result.structure);
+
+if(result.structure){
+
+html += createStructureCard(
+result.structure
+);
+
+}
 
 
-html+=createWuxingCard(result.wuxing);
+
+if(result.wuxing){
+
+html += createWuxingCard(
+result.wuxing
+);
+
+}
 
 
-html+=createStrengthCard(result.strength);
+
+if(result.strength){
+
+html += createStrengthCard(
+result.strength
+);
+
+}
 
 
 
@@ -43,32 +66,62 @@ return `
 
 <section class="report-card">
 
-
 <h2>
 格局分析
 </h2>
 
 
 <h3>
-
 ${structure.structure}
-
 </h3>
 
 
 <p>
-
 月令：
 ${structure.monthZhi}
-
 </p>
 
 
 <p>
-
 ${structure.description}
-
 </p>
+
+
+</section>
+
+
+`;
+
+}
+
+
+
+
+
+
+function createDayMasterCard(result){
+
+
+return `
+
+
+<section class="report-card">
+
+
+<h2>
+日主
+</h2>
+
+
+<div class="daymaster">
+
+${result.dayMaster.stem}
+
+<span>
+${result.dayMaster.element}
+</span>
+
+</div>
 
 
 </section>
